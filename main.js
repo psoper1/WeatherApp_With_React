82eaee09da3462d98c7f4bcc8f880223d36b2f42
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-18 18:46:43 +0000

    more stuff2

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=82d10d8b9124f9ece3e56c62befeda72`;
-let zipCode = "inputField";
-let btn = "submitZip";
+let url = `https://api.openweathermap.org/data/2.5/weather?zip=40515&appid=82d10d8b9124f9ece3e56c62befeda72`;
+let zipCode = document.getElementById("inputField");
+let btn = document.getElementById("submitZip");
 let cityInfo = document.getElementById("city");
 let wTemp = document.getElementById("temperature");
 let wConditions = document.getElementById("wConditions");
 
-let state = [{
-    cityInfo: "",
-    wTemp: "",
-    wConditions: "",
-}]
 
+
+// let state = [{
+//     cityInfo: "",
+//     wTemp: "",
+//     wConditions: "",
+// }]
+
+zipCode.addEventListener('click', function getAPIdata() {
+
+    axios.get(url)
+        .then(response => {
+            cityInfo = response.data.name;
+            console.log(response.data.name);
+
+        })
+        .catch(error => alert("Looks like you picked yourself a bouquet of Oopsie Daisies and didn't enter a valid ZIP Code!", error));
+
+})
